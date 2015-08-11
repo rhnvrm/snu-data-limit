b9e8fcebebcc9c86ef2dbe1b67595131da9e45c3
rhnvrm <rohanverma2004@gmail.com>
2015-08-11 13:01:10 +0530

    completed the script

---
+++ consumption.js
@@ -0,0 +1,30 @@
+var sum = 0.0;
+var date_n = new Date(); //current time
+var currentTime = date_n;
+var currentDay = currentTime.getDay();
+var wednesday = 3;
+var daysBack, pastDate;
+if(currentDay > wednesday){
+	daysBack = currentDay - wednesday;
+}
+else{
+	daysBack = (7 - wednesday) + currentDay;
+}
+
+last_wednesday = new Date(date_n.getFullYear(),date_n.getMonth(), date_n.getDate() - daysBack);
+	console.log(last_wednesday);
+for (i = 18; i < document.getElementsByTagName('tr').length - 4; i++){
+
+	d = parseFloat(document.getElementsByTagName('tr')[i].childNodes[6].innerHTML.split(' ')[0]);
+
+	date = new Date(document.getElementsByTagName('tr')[i].childNodes[3].innerHTML + ", 2015");
+
+
+
+	if(date > last_wednesday){	
+		sum += d;
+		//console.log(date);
+	}
+
+}
+alert("downloaded = " + sum);
